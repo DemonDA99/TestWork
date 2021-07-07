0e77fd2cb23c2b7f81786461f53bcd33045b3a7c
DemonDA99 <nikita.mishin.1999@yandex.ru>
2021-07-07 21:24:58 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TestWork
+{
+    partial class FormOne
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormOne));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuinfoProgramme = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFile,
+            this.MenuinfoProgramme});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(597, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenuFile
+            // 
+            this.MenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuOpen,
+            this.MenuExit});
+            this.MenuFile.Name = "MenuFile";
+            this.MenuFile.Size = new System.Drawing.Size(48, 20);
+            this.MenuFile.Text = "Файл";
+            // 
+            // MenuOpen
+            // 
+            this.MenuOpen.Name = "MenuOpen";
+            this.MenuOpen.Size = new System.Drawing.Size(121, 22);
+            this.MenuOpen.Text = "Открыть";
+            this.MenuOpen.Click += new System.EventHandler(this.MenuOpen_Click);
+            // 
+            // MenuExit
+            // 
+            this.MenuExit.Name = "MenuExit";
+            this.MenuExit.Size = new System.Drawing.Size(121, 22);
+            this.MenuExit.Text = "Выход";
+            this.MenuExit.Click += new System.EventHandler(this.MenuExit_Click);
+            // 
+            // MenuinfoProgramme
+            // 
+            this.MenuinfoProgramme.Name = "MenuinfoProgramme";
+            this.MenuinfoProgramme.Size = new System.Drawing.Size(94, 20);
+            this.MenuinfoProgramme.Text = "О программе";
+            this.MenuinfoProgramme.Click += new System.EventHandler(this.MenuinfoProgramme_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(206, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(379, 249);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.DimGray;
+            this.imageList1.Images.SetKeyName(0, "Folder_29922.png");
+            this.imageList1.Images.SetKeyName(1, "iconfinder-document06-1622830_121956.png");
+            // 
+            // treeView1
+            // 
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(12, 36);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(188, 249);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // FormOne
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 297);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormOne";
+            this.Text = "Открытие и загрузка картинок";
+            this.Load += new System.EventHandler(this.FormOne_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenuFile;
+        private System.Windows.Forms.ToolStripMenuItem MenuOpen;
+        private System.Windows.Forms.ToolStripMenuItem MenuExit;
+        private System.Windows.Forms.ToolStripMenuItem MenuinfoProgramme;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWork
+{
+    public partial class FormOne : Form
+    {
+        public FormOne()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuExit_Click(object sender, EventArgs e)
+        {
+            //Закрытие программы через меню
+            this.Close();
+        }
+
+        private void MenuOpen_Click(object sender, EventArgs e)
+        {
+            //Очистка формы
+            //Выбор и отображение папок из диалогового окна
+            treeView1.Nodes.Clear();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var rootDirectoryInfo = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+                treeView1.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            switch (e.Node.Text)
+            {
+                case "Пейзаж горы.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Пейзаж\\Пейзаж горы.jpg");
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case "Пейзаж луга.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Пейзаж\\Пейзаж луга.jpg");
+                    break;
+                case "Пейзаж озеро.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Пейзаж\\Пейзаж озеро.jpg");
+                    break;
+                case "Пейзаж с лодкой.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Пейзаж\\Пейзаж с лодкой.jpg");
+                    break;
+                case "Пейзаж туман.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Пейзаж\\Пейзаж туман.jpg");
+                    break;
+                case "Нью-Йорк.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Города\\Нью-Йорк.jpg");
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case "Москва.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Города\\Москва.jpg");
+                    break;
+                case "Париж.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Города\\Париж.jpg");
+                    break;
+                case "Сидней.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Города\\Сидней.jpg");
+                    break;
+                case "Ауди.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Машины\\Ауди.jpg");
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case "Феррари.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Машины\\Феррари.jpg");
+                    break;
+                case "Ламба.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Машины\\Ламба.jpg");
+                    break;
+                case "Астон.jpg":
+                    pictureBox1.Image = Image.FromFile("D:\\С#\\WindowsForms\\TestWork\\Изображения\\Машины\\Астон.jpg");
+                    break;
+            }
+        }
+
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            //Создание дерева для выбора папок с изображениями
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            foreach (var file in directoryInfo.GetFiles())
+                directoryNode.Nodes.Add(new TreeNode(file.Name));
+            return directoryNode;
+        }
+        private void FormOne_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void MenuinfoProgramme_Click(object sender, EventArgs e)
+        {
+            //Вывод сообщения о программе 
+            MessageBox.Show(
+                "Приложение по загрузке и отображению картинок \nиз локальной папки." +
+                "\nСозданно в рамках тестового задания." +
+                "\n\nВыполнил: Мишин Н.А",
+                "О программе");
+                
+        
+        }
+    }
+}
